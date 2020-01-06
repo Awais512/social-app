0cff8a6e51a555c3971dc16372d4656e2685ba8b
awais512 <raza.awais25@gmail.com>
2020-01-06 22:51:01 +0500

    Validation and Login Route

--- functions/index.js
+++ functions/index.js
@@ -20,6 +20,7 @@ const firebase = require('firebase');
 firebase.initializeApp(config);
 const db = admin.firestore();
 
+//Get All Screams Route
 app.get('/screams', (req, res) => {
   db.collection('screams')
     .orderBy('createdAt', 'desc')
@@ -39,6 +40,7 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err));
 });
 
+//Create Scream Route
 app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
@@ -58,6 +60,19 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = string => {
+  if (string.trim() === '') return true;
+  else return false;
+};
+
+//Signup Route
 app.post('/signup', (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -65,6 +80,23 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
+
+  //Errors Object
+  let errors = {};
+
+  //Validations
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Email must not be Empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid Email address';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Password must not be Empty';
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = 'Password must match';
+  if (isEmpty(newUser.handle)) errors.handle = 'Handle Can not be Empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
@@ -99,6 +131,40 @@ app.post('/signup', (req, res) => {
       console.error(err);
       if (err.code === 'auth/email-already-in-use') {
         return res.status(400).json({ email: 'Email Already in Use' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) errors.email = 'Email must not be empty';
+  if (isEmpty(user.password)) errors.email = 'Password must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === 'auth/wrong-password') {
+        return res
+          .status(403)
+          .json({ generals: 'Wrong Credentials, Please try again' });
       } else {
         return res.status(500).json({ error: err.code });
       }
