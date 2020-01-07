86f9a26d2e4f080cf998adb8058e71add994a38f
awais512 <raza.awais25@gmail.com>
2020-01-07 13:55:59 +0500

    Refactoring and Organizing

---
+++ functions/utils/config.js
@@ -0,0 +1,11 @@
+//Firebase Configuration Object
+module.exports = {
+  apiKey: 'AIzaSyDKKU68t30i5HgHj9WKg3g5qDR24slYeOk',
+  authDomain: 'social-acfbe.firebaseapp.com',
+  databaseURL: 'https://social-acfbe.firebaseio.com',
+  projectId: 'social-acfbe',
+  storageBucket: 'social-acfbe.appspot.com',
+  messagingSenderId: '700007891489',
+  appId: '1:700007891489:web:329a87aaabca1f711f5960',
+  measurementId: 'G-6QEKST8QFX'
+};
