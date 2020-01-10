154a3aedd5be6338beca2e7f4f86dbee060c995d
awais512 <raza.awais25@gmail.com>
2020-01-10 16:24:36 +0500

    Comments on post

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,12 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 const FBAuth = require('./utils/fbAuth');
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require('./handlers/screams');
 const {
   signup,
   login,
@@ -14,12 +19,14 @@ const {
 //Screams Route
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/scream/:screamId', getScream);
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 //Users Routes
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
